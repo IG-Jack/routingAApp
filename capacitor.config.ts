895a1ed1d6636b352cf4f62f08c5999f7f8cc5f5
GOJoelDavid <davichogranda@gmail.com>
2023-12-01 06:47:25 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'routingapp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
